6c118b7b84d18678e1a0b862e67e3a23866bd691
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-26 20:32:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
 
+def initialize(name)
+  @name = name
+  @@all << self
+end 
   def save
     self.class.all << self
   end
