9bad33434b3b4c70f832fa4623ccf151daf40c2e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-26 20:48:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,53 +11,62 @@ class Song
     self.class.all << self
   end
 
-#instantiates and saves the song, and it returns the new song that was created
-def self.create
-  song = self.new
-  self.all << song #class constructor
-  song
-end
-
-#instantiates a song with a name property
-def self.new_by_name(name) #class constructor 
-  song = self.new
-  song.name = name
-  song
-end
-
-#can find a song present in @@all by name
-#returns falsey when a song name is not present in @@all
-def self.find_by_name(name)
-  @@all.find{ |x| x.name == name}
-end
-
-#invokes .find_by_name and .create_by_name instead of repeating code
-#returns the existing Song object (doesn't create a new one) when provided the title of an existing Song
-self.find_by_name(name) || self.create_by_name(name)
-end
-
-def self.alphabetical()
-  @@all.sort_by{|x| x.name}
-end
-
-def self.new_from_filename(name)
-  song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+  def self.create #class constructor
+    song = self.new
+    self.all << song
     song
   end
 
-  def self.create_from_filename(name)
-      song = self.new
-      song.name = (name.split(" - ")[1].chomp(".mp3"))
-      song.artist_name = (name.split(" - ")[0])
-      @@all << song
-      song
+  def self.new_by_name(title) #class constructor
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title) #class constructor
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title) #class finder
+    result = self.all.detect {|song| song.name == title}
+    result
+  end
+
+  def self.find_or_create_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
     end
+  end
 
-def self.destroy_all()
-  @@all.clear
-end
+  def self.alphabetical
+    sorted = self.all.sort_by {|song| song.name}
+    sorted
+  end
 
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
 
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
